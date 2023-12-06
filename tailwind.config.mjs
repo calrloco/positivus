15e07e4360bfa2b55eabe5081bb2625d45752e66
carlo briganti <60385209+calrloco@users.noreply.github.com>
2023-12-06 12:30:27 +0100

    hero mobile h1 + image button to put in component

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,14 +1,20 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {
-			colors: {
-				'brand-primary': '#B9FF66',
-				'brand-slate': '#B9FF66',
-				'brand-dark': '#B9FF66'
-			}
-		},
-	},
-	plugins: [],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      colors: {
+        "brand-primary": "#B9FF66",
+        "brand-slate": "#B9FF66",
+        "brand-dark": "#191A23",
+      },
+      borderRadius: {
+        'btn': '14px'
+      },
+    },
+    fontFamily: {
+      sans: ["Space Grotesk", "VT323", "sans-serif"],
+    },
+  },
+  plugins: [],
+};
